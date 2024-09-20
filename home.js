c47c3e686e1c1c59d8b63d19eabaa2d06a4ee50e
asifsarkar01 <aassiiff1155@gmail.com>
2024-09-20 11:15:07 -0500

    four

--- home.js
+++ home.js
@@ -12,6 +12,25 @@
             const getPin =document.getElementById('get-pin').value;
             console.log(getPin);
 
+            // step-4 verify pin number (pin number)sothik ache kina ta check kore console a result paowar jonno o tk balance add korar way 
+            // but this is wrong way eita begginer der jonno 
+            if(getPin === '1234'){
+              console.log('click this pin i found this')
+            //   get the current balance 
+         const balance= document.getElementById('balance-stat').innerText;
+         console.log(balance)
+
+        //  step-5 add addmoney input with balance 
+       const addAmountNumber = parseFloat(addAmount);
+       const balanceNumber = parseFloat(balance);
+       const newBalance =addAmountNumber+balanceNumber;
+       console.log(newBalance);
+    //  step-6  update balance uprer available balance a new balance add korar jonno available balace er id stat oita ante hobe abong inner text er por newBalace bosate hobe 
+       document.getElementById('balance-stat').innerText=newBalance;
+            }
+            else{
+                alert('Wrong pin number! try again!')
+            }
         })
    
     
